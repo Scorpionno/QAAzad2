ca31a4339a3557bab55eb4693aac855fbf0577d2
Scorpionno <jigoku27772@gmail.com>
2024-02-29 22:19:46 +0100

    Koniec

--- cypress/e2e/zadanie2.cy.js
+++ cypress/e2e/zadanie2.cy.js
@@ -2,15 +2,15 @@ describe('User can visit GoIT page', () => {
     beforeEach('go to page', () => {
         cy.visit('https://www.edu.goit.global/account/login');
     });
-//TEST 2
-    it('successfully login to the GoIT page', () => {
+//TEST 1
+    it('successfully login to the GoIT page TEST 1', () => {
         cy.logInUser('user888@gmail.com', '1234567890');
         cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
         cy.get('button[class="next-7afvtf e1phyiqy6"]').click().wait(5000);
         cy.contains('Log out').click().wait(5000);
     })
- //TEST 1
-    it('successfully login to the GoIT page second test', () => {
+ //TEST 2
+    it('successfully login to the GoIT page TEST 2', () => {
       cy.logInUser('testowyqa@qa.team', 'QA!automation-1').wait(5000);
       cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
       cy.get('button[class="next-7afvtf e1phyiqy6"]').click();
