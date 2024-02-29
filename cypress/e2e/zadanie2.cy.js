07479d83627267537346894287b9fb410d33e2a9
Scorpionno <jigoku27772@gmail.com>
2024-02-29 21:59:55 +0100

    test 1

--- cypress/e2e/zadanie2.cy.js
+++ cypress/e2e/zadanie2.cy.js
@@ -3,10 +3,11 @@ describe('User can visit GoIT page', () => {
         cy.visit('https://www.edu.goit.global/account/login');
     });
 
+//Pierwszy test
     it('successfully login to the GoIT page', () => {
         cy.logInUser('user888@gmail.com', '1234567890');
         cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
-        cy.get('button[class="next-7afvtf e1phyiqy6"]').click().wait(5000);
+        cy.get('button[class="next-7afvtf e1phyiqy6"]').click()
         cy.contains('Log out').click();
     });
 });
